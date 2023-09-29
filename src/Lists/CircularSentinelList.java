5591c8c922a585187fb9fddff97d5f13eb8ba156
riteshkhatri08 <riteshkhatri1512@gmail.com>
2023-09-29 23:16:35 +0530

    IMplemented Circular Sentinel List

---
+++ src/Lists/CircularSentinelList.java
@@ -0,0 +1,22 @@
+package Lists;
+
+public class CircularSentinelList {
+
+    SentinelNode head;
+
+    public CircularSentinelList() {
+
+        this.head = new SentinelNode(null);
+        this.head.next = this.head.prev = this.head;
+
+    }
+
+    public static void main(String[] args) {
+        CircularSentinelList list = new CircularSentinelList();
+        list.addToBack(3);
+    }
+
+    private void addToBack(int value) {
+    }
+
+}
