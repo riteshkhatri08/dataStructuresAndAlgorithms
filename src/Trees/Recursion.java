9f682ed97d18a2016498cbd30f4ae6e33c20584c
riteshkhatri08 <riteshkhatri1512@gmail.com>
2023-09-29 23:17:18 +0530

    Demonstrate Factorial using recursion

---
+++ src/Trees/Recursion.java
@@ -0,0 +1,16 @@
+package Trees;
+
+public class Recursion {
+    public static void main(String[] args) {
+        int n = 5;
+
+        System.out.println(n + "! = " + factorial(n));
+    }
+
+    private static int factorial(int n) {
+
+        // Recusive Call
+        return n == 0 ? 1 : (n * factorial(n - 1));
+
+    }
+}
